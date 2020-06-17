3f2259d20138a8e8a33482d1726a732e6bcfa849
WeeNews <wenqiangxin@gmail.com>
2020-06-17 10:00:08 +0800

    init

---
+++ src/test/java/com/iplab/xwq/fblog/FblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iplab.xwq.fblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
